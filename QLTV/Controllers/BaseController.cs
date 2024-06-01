c032327594e0d1f5b0604527bfb81d8fc9237507
MaiHuyHoat <hoatdfk2001@gmail.com>
2024-06-01 15:00:58 +0700

    first commit

---
+++ QLTV/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QLTV.Data.Model;
+
+namespace QLTV.Controllers
+{
+    public class BaseController<T> : Controller
+    {
+        protected manage_libraryContext _DbContext;
+        protected Logger<T> _logger;
+
+        public BaseController(manage_libraryContext dbContext, Logger<T> logger)
+        {
+            _DbContext = dbContext;
+            _logger = logger;
+        }
+    }
+}
---
+++ QLTV/Data/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+namespace QLTV.Data
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string? message { get; set; }
+    }
+}
